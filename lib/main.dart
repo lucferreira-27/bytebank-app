aac23ec7c475b984e74131311379ae7b711ed0dc
Lucas Ferreira D. Silva <lucferreira14@gmail.com>
2021-03-09 21:59:10 -0300

    Implementando layout com Widgets

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,54 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/src/widgets/framework.dart';
 
-void main() {
+void main() => runApp(
+      MaterialApp(
+        home: Scaffold(
+          body: ListaTranferencia(),
+          appBar: AppBar(
+            title: Text("Transferência"),
+          ),
+          floatingActionButton: FloatingActionButton(
+            child: Icon(Icons.add),
+            onPressed: null,
+          ),
+        ),
+      ),
+    );
 
-
+class ListaTranferencia extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        ItemTransferencia(Transferencia(200, 213)),
+        ItemTransferencia(Transferencia(300, 243)),
+        ItemTransferencia(Transferencia(400, 293))
+      ],
+    );
+  }
 }
 
+class ItemTransferencia extends StatelessWidget {
+  final Transferencia _transferencia;
+
+  ItemTransferencia(this._transferencia);
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      child: ListTile(
+        leading: Icon(Icons.monetization_on),
+        title: Text(_transferencia.contaNumro.toString()),
+        subtitle: Text(_transferencia.valor.toString()),
+      ),
+    );
+  }
+}
+
+class Transferencia {
+  final double valor;
+  final int contaNumro;
+
+  Transferencia(this.valor, this.contaNumro);
+}
